ee7b690025f55dce71cdaf67bf66ff7ca79fe45a
xAdministrator <xAdministrator@BalakrishnanM1.ad.local>
2022-02-24 22:56:59 +0530

    adding server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app  = express();
+const path = require('path')
+const history = require('connect-history-api-fallback')
+
+const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname ,'/dist'))
+})
+app.listen(80, function(err, res) {
+  if(!err)
+   console.log(`App started listening on the port 80`)
+});
